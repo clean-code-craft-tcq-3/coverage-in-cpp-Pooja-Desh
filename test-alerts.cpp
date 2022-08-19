13ffa980abaab3f89e74e4d6a10eeff59ba66952
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-19 21:04:18 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,44 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits") 
+{
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(-12, 0, 35) == TOO_LOW);
+  REQUIRE(inferBreach(-12, 0, 40) == TOO_LOW);
+  REQUIRE(inferBreach(-12, 0, 45) == TOO_LOW);
+
+  REQUIRE(inferBreach(47, 0, 35) == TOO_HIGH);
+  REQUIRE(inferBreach(30, 0, 35) == NORMAL);
+
+  REQUIRE(inferBreach(45, 0, 40) == TOO_HIGH);
+  REQUIRE(inferBreach(35, 0, 40) == NORMAL);
+
+  REQUIRE(inferBreach(47, 0, 45) == TOO_HIGH);
+  REQUIRE(inferBreach(30, 0, 45) == NORMAL);
+
 }
+
+TEST_CASE("Temperature breach acoording to cooling type")
+{
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -3) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 25) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == TOO_HIGH);
+
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -10) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 31) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 43) == TOO_HIGH);
+
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -5) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 43) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 47) == TOO_HIGH);
+
+}
+
+TEST_CASE("Alerter via email and controller")
+{
+  REQUIRE(checkAndAlert(TO_EMAIL, MED_ACTIVE_COOLING, 43) == true);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, HI_ACTIVE_COOLING, 43) == true);
+}
+
+
