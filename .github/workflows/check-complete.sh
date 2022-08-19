ddaf5ebd02a8b7c188333cceb64ebc89dc8f7d20
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-19 21:32:21 +0530

    Update check-complete.sh

--- .github/workflows/check-complete.sh
+++ .github/workflows/check-complete.sh
@@ -2,6 +2,6 @@
 set -e
 
 if grep -q _enter *.md; then
-  echo "Replace all text having (cyclomatic complexity can be surely reduced with this approach in my working project) with your input"
+  echo "Replace all text having trial_enter with your input"
   exit 1
 fi
